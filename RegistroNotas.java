f87b4fce8889c520571e798b647200ef0e618adb
Jose Apestey <jopy70751gmail.com>
2023-03-26 17:28:19 -0300

    primer comit desde login de jopo

--- RegistroNotas.java
+++ RegistroNotas.java
@@ -17,17 +17,17 @@ public class RegistroNotas {
         double[] promedios = new double[10];
 
         
-        Scanner leer = new Scanner(System.in);
+        Scanner lee = new Scanner(System.in);
 
         
         for (int i = 0; i < 10; i++) {
             System.out.println("Ingrese las notas del alumno " + (i+1));
 
             
-            double nota1 = leer.nextDouble() * 0.1;
-            double nota2 = leer.nextDouble() * 0.15;
-            double nota3 = leer.nextDouble() * 0.25;
-            double nota4 = leer.nextDouble() * 0.5;
+            double nota1 = lee.nextDouble() * 0.1;
+            double nota2 = lee.nextDouble() * 0.15;
+            double nota3 = lee.nextDouble() * 0.25;
+            double nota4 = lee.nextDouble() * 0.5;
             double promedio = nota1 + nota2 + nota3 + nota4;
 
            
